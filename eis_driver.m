e21ad9afa9ca5c61ae9f6b40e1e28ad2ef5ebe15
silvers <silvers@princeton.edu>
2016-12-09 00:51:20 -0500

    many changes to eis.m it might even work now

--- eis_driver.m
+++ eis_driver.m
@@ -3,6 +3,14 @@
 % 
 % this script computes the LTS and EIS using equation 4 and 5
 %
+% do I want to make this into a function that outputs LTS and EIS?
+% if so it could work nicely to have the input from the function be the 
+% source for the experimental data and the output being the LTS and EIS.
+%
+% with these as output it would be simple to plot LTS and EIS either 
+% directly, or to compute differences between LTS and EIS and then use
+% cont_wcolorbar_eis to plot the differences.  
+%
 % levi silvers                                 dec 2016
 %
 Lv=2.500*10^6 % J/kg
@@ -15,20 +23,6 @@ p0=100000. % Pa
 rh0=0.8
 kappa=Ra/cp;
 
-% moist-adiabatic potential temperature gradient gamma_m
-temp=273.15:1:373.15;
-press=100:5:1000;
-
-% compute the Lower Tropospheric Stability (LTS)
-% (theta_700-theta_0)
-p_z=700;
-theta0=temp.*(1.0)^kappa;
-theta=temp.*(p_z/p0)^kappa;
-lts=theta0-theta;
-
-% lts 
-% there are difficulties in computing theta where land is present because 
-%
 %theta_f=zeros(23,180,288);
 %temp3=zeros(180,288);
 theta_f=zeros(288,180,23);
@@ -52,22 +46,6 @@ lts_f=zeros(288,180);
 %lts_f=theta_f(5,:,:)-theta_f(1,:,:);
 lts_f=theta_f(:,:,5)-theta_f(:,:,1);
 
-% compute the saturation mixing ratio qs=0.622*es/(p-es)
-es=6.11*exp((Lv/Rv)*((1./273.15)-(1./temp)));
-%qs=0.622.*es/.(p-es);
-for i=1:181;
-  for j=1:61;
-    es_temp=6.11*exp((Lv/Rv)*((1./273.15)-(1./temp(j))));
-    qs(j,i)=0.622*es_temp/(press(i)-es_temp);
-  end
-end
-
-gamma_m=zeros(size(temp,2),size(press,2));
-for i=1:181;
-  for j=1:61;
-    gamma_m(j,i)=(g/cp)*(1-(1+Lv*qs(j,i)/(Ra*temp(j)))/(1+Lv*Lv*qs(j,i)/(cp*Rv*temp(j)*temp(j))));
-  end
-end
 
 % compute gamma_m_850
 %t_850=(v.temp(1,:,:)+v.temp(5,:,:))/2.;
@@ -83,14 +61,18 @@ gamma_m_850=(g/cp)*(1-(1+Lv*qs_850./(Ra*t_850))./(1+Lv*Lv*qs_850./(cp*Rv*t_850.*
 % compute an approximate lifting condensation level (lcl)
 %lcl=(20.+(temp-273.15)/5.)*(1.-rh0)*100.;
 % using only surface values
-lcl=(20.+(T0-273.15)/5.)*(1.-rh0)*100.;
+%lcl=(20.+(T0-273.15)/5.)*(1.-rh0)*100.;
+lcl=(20.+(v.tsurf-273.15)/5.)*(1.-rh0)*100.;
 
-% and now for the estimated inversion strength: estinvs
-% eis=lts - gamma_m(850)*(z(700)-LCL);
-% what value should be used for the height of the 700 mb pressure surface?  
 %
 %estinvs=lts_f-gamma_m_850*(squeeze(hght(5,:,:))-lcl);
 estinvs=lts_f-gamma_m_850.*(squeeze(v.hght(:,:,5))-lcl);
 %
+% plot the LTS and EIS
 %
-%%gamma_m=zeros(size(temp,2),size(press,2));
+cont_wcolorbar_eis(lts_f')
+cont_wcolorbar_eis(estinvs')
+%
+%
+%
+%
--- openncfile_new.m
+++ openncfile_new.m
@@ -1,16 +1,21 @@
-source='/Users/silvers/data/cre_sstpatt/c96L32_am4g10r8_2000climo_av10yr_atmos_subsvar.0002-0011.all.nc'
+%----------------------------------------------------------------
+% scripts to read and open data from a netcdf file into Matlab
+%
+% this should work for Matlab 2015 as used on the macbook
+%
+% levi silvers                     dec 2016
+%----------------------------------------------------------------
+%source='/Users/silvers/data/cre_sstpatt/c96L32_am4g10r8_2000climo_av10yr_atmos_subsvar.0002-0011.all.nc'
+source='/Users/silvers/data/cre_sstpatt/c96L32_am4g10r8_2000climo_atmos.0002-0021.07.nc'
 ncid=netcdf.open(source);
 [numdims,nvars,natts]=netcdf.inq(ncid);
 nvars
-%[varname,xtype,dimids,atts]=netcdf.inqVar(ncid,21)
-%vardatatest=ncread(source,tot_cld_amt);
-[tcld,xtype,dimids,atts]=netcdf.inqVar(ncid,21);
-vardatatest=ncread(source,tcld);
-size(vardatatest);
-
-for ii=1:22
-	%varid=netcdf.inqVarID(ncid,varname);
-	varid=int2str(ii);
-	[varname,xtype,dimids,atts]=netcdf.inqVar(ncid,varid);
-        varname
-end
+%[tcld,xtype,dimids,atts]=netcdf.inqVar(ncid,21);
+%vardatatest=ncread(source,tcld);
+v.lat=ncread(source,'lat');
+v.lon=ncread(source,'lon');
+v.level=ncread(source,'level');
+v.level=100.*v.level;
+v.temp=ncread(source,'temp');
+v.tsurf=ncread(source,'t_surf');
+v.hght=ncread(source,'hght');
