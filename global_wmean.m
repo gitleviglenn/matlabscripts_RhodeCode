53eda917f2ffa11a9dfad4d5eab0001504198441
silvers <silvers@princeton.edu>
2017-01-26 16:00:52 -0500

    global_wmean.m and cont_map.m to compute lat weighted means, and to create a contour map

--- global_wmean.m
+++ global_wmean.m
@@ -2,7 +2,8 @@ function out_var=global_wmean(fieldin,vlon,vlat)
 %------------------------------------------------------------
 % levi silvers                                 oct 2016
 %------------------------------------------------------------
-
+%clear longit latit latitweight glblatweight nlongit
+%clear glbsumweight wgt_var out_var
 
 longit=vlon;
 latit=vlat;
@@ -16,7 +17,18 @@ end
 %glbsumweight=sum(glblatweight(:));
 glbsumweight=nansum(glblatweight(:));
 
-in_var=fieldin;
-wgt_var       = in_var(:,:).*glblatweight;
-%out_var = sum(wgt_var(:))/glbsumweight
-out_var = nansum(wgt_var(:))/glbsumweight;
+out_var=glblatweight;
+
+%sumone=sum(glblatweight);
+%size(sumone)
+%sumtwo=sum(sumone);
+%size(sumtwo)
+%sumtwo
+%
+%glbsumweight
+%
+%in_var=fieldin;
+%%wgt_var       = in_var(:,:).*glblatweight;
+%wgt_var       = in_var.*glblatweight;
+%%out_var = sum(wgt_var(:))/glbsumweight
+%out_var = nansum(wgt_var(:))/glbsumweight;
