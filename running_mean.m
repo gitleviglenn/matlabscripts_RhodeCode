f78770b6430b391e504d73501f6de99d3ef06bf0
silvers <silvers@princeton.edu>
2017-05-05 18:15:34 -0400

    running_mean.m

---
+++ running_mean.m
@@ -0,0 +1,21 @@
+
+%-----------------------------------------------------------------------------------------
+%tend_a is presumably the end of the series
+% tend_a = iend_a-istart_a;
+%incoming_ts=v.sst_mn_ts_a;
+% compute a running mean
+rough_ts=incoming_ts;
+%tendindex=tend_a;
+% for a 13 point running mean
+%for ti=7:tendindex-6
+%  ts_smooth(ti-6)=(rough_ts(ti-6)+rough_ts(ti-5)+rough_ts(ti-4)+rough_ts(ti-3)+rough_ts(ti-2)+rough_ts(ti-1)+rough_ts(ti)+rough_ts(ti+1)+rough_ts(ti+2)+rough_ts(ti+3)+rough_ts(ti+4)+rough_ts(ti+5)+rough_ts(ti+6))/13.;
+%end
+% for a 9 point running mean
+for ti=5:tendindex-4
+  ts_smooth(ti-4)=(rough_ts(ti-4)+rough_ts(ti-3)+rough_ts(ti-2)+rough_ts(ti-1)+rough_ts(ti)+rough_ts(ti+1)+rough_ts(ti+2)+rough_ts(ti+3)+rough_ts(ti+4))/9.;
+end
+output_ts=ts_smooth;
+clear rough_ts; clear ts_smooth;
+%-----------------------------------------------------------------------------------------
+%sfc_t_sm_ts_a=output_ts;
+%
