acae68c428d2448424543c5be7d171fada1fde99
silvers <silvers@princeton.edu>
2017-05-24 17:07:39 -0400

    lcc_mlr.m lcc_mlr_driver.m

--- lcc_mlr.m
+++ lcc_mlr.m
@@ -2,11 +2,16 @@
 %-------------------------------------------------------------------------------
 % use multiple linear regression to estimate the dependence of changes in low cloud cover (lcc) on change of the estimated inversion strength (eis) and the change in sst.  
 %
+% primary_ts = alpha*first_ts + betta*second_ts + residual?  
+%
 % time series are obtained from 
 % driver_ensembles, using readvars and eis_lts_ts.
 %
 % i think that we should be using the anomalous time series for the linear regresion.  
 % the anomalous time series is computed here.
+%
+% do i need to first remove the seasonal cycle?  ming 
+% says this should be the same as using the yearly mean values.  
 %
 % levi silvers                                            may 2017
 %-------------------------------------------------------------------------------
@@ -15,17 +20,9 @@
 % sst here could mean sst, tref, or low level temp
 % i am a sloppy man.
 
-%%sst_tr=temp_ll_ts(:,60:120,:);
-%%eis_tr=eis_array_am4(:,60:120,:);
-%%lcc_tr=lcloud_array_am4(:,60:120,:);
-%
-%incoming_temp_ts=temp_ll_ts(:,60:120,:);
-%incoming_eis_ts=eis_array_am4(:,60:120,:,1);
-%incoming_lcc_ts=lcloud_array_am4(:,60:120,:,1);
-
-sst_tr=incoming_temp_ts;%(:,60:120,:);
-eis_tr=incoming_eis_ts;%(:,60:120,:);
-lcc_tr=incoming_lcc_ts;%(:,60:120,:);
+sst_tr=second_ts;%(:,60:120,:);
+eis_tr=first_ts;%(:,60:120,:);
+lcc_tr=primary_ts;%(:,60:120,:);
 
 % average over the tropics
 sst_tr_mn_a=mean(sst_tr,3);
@@ -49,11 +46,11 @@ tindex=size(temp_ll_ts,1);
 nyears=tindex/12;
 clear monthsbyyears
 monthsbyyears=reshape(eis_del,[12 nyears]);
-eis_del_ymn=mean(monthsbyyears,1);
+first_del_ymn=mean(monthsbyyears,1);
 monthsbyyears=reshape(sst_del,[12 nyears]);
-sst_del_ymn=mean(monthsbyyears,1);
+second_del_ymn=mean(monthsbyyears,1);
 monthsbyyears=reshape(lcc_del,[12 nyears]);
-lcc_del_ymn=mean(monthsbyyears,1);
+primary_del_ymn=mean(monthsbyyears,1);
 
 %monthsbyyears=reshape(eis_tr_mn,[12 nyears]);
 %eis_tr_ymn=mean(monthsbyyears,1);
@@ -66,8 +63,8 @@ lcc_del_ymn=mean(monthsbyyears,1);
 %var_array2=[eis_del sst_del];
 %cc=mvregress(var_array2,lcc_del);
 
-var_array=[eis_del_ymn' sst_del_ymn'];
-cc=mvregress(var_array,lcc_del_ymn')
+var_array=[first_del_ymn' second_del_ymn'];
+cc=mvregress(var_array,primary_del_ymn')
 
 
 
--- lcc_mlr_driver.m
+++ lcc_mlr_driver.m
@@ -3,12 +3,20 @@
 % calls lcc_mlr.m to use multiple linear regression in the decomposition
 % of the influence of SST, and EIS on LCC for AM2, AM3, and AM4
 %
+% what does this need to have run before it is called?  
+% it does not work after ensmn_ncout.m
+%
 % levi silvers                                        may 2017
 %------------------------------------------------------------------------
 
-incoming_temp_ts=temp_sfc_am2_mn(:,30:60,:);
-incoming_eis_ts=eis_ens_am2_mn(:,30:60,:);
-incoming_lcc_ts=lcloud_am2_mn(:,30:60,:);
+%incoming_temp_ts=temp_sfc_am2_mn(:,30:60,:);
+%incoming_eis_ts=eis_ens_am2_mn(:,30:60,:);
+%incoming_lcc_ts=lcloud_am2_mn(:,30:60,:);
+
+%am2
+second_ts=temp_sfc_am2_mn(:,30:60,:);
+first_ts=eis_ens_am2_mn(:,30:60,:);
+primary_ts=lcloud_am2_mn(:,30:60,:);
 
 ensnum=1;
 
@@ -20,59 +28,136 @@ clear var_array;
 clear cc;
 lcc_mlr
 
+incoming_ts=primary_del_ymn;
+running_mean;
+lcc_am2_smooth=output_ts;
+
 figure
-plot(lcc_del_ymn,'k')
+plot(primary_del_ymn,'k')
 hold on
-zhou_ts=3.7.*eis_del_ymn-0.9.*sst_del_ymn;
+zhou_ts=3.7.*first_del_ymn-0.9.*second_del_ymn;
 plot(zhou_ts,'g')
-new_ts=cc(1).*eis_del_ymn+cc(2).*sst_del_ymn;
+new_ts=cc(1).*first_del_ymn+cc(2).*second_del_ymn;
 plot(new_ts,'r')
-%zhou_am2=corrcoef(lcc_del_ymn,zhou_ts)
-%new_am2=corrcoef(lcc_del_ymn,new_ts)
 title('lcc change for AM2')
 
-incoming_temp_ts=temp_sfc_am3_mn(:,30:60,:);
-incoming_eis_ts=eis_ens_am3_mn(:,30:60,:);
-incoming_lcc_ts=lcloud_am3_mn(:,30:60,:);
-%incoming_temp_ts=temp_sfc_array_am3(:,30:60,:,ensnum);
-%incoming_eis_ts=eis_array_am3(:,30:60,:,ensnum);
-%incoming_lcc_ts=lcloud_array_am3(:,30:60,:,ensnum);
+incoming_ts=new_ts;
+running_mean;
+new_ts_am2_smooth=output_ts;
+
+%am3
+second_ts=temp_sfc_am3_mn(:,30:60,:);
+first_ts=eis_ens_am3_mn(:,30:60,:);
+primary_ts=lcloud_am3_mn(:,30:60,:);
+
+%incoming_temp_ts=temp_sfc_am3_mn(:,30:60,:);
+%incoming_eis_ts=eis_ens_am3_mn(:,30:60,:);
+%incoming_lcc_ts=lcloud_am3_mn(:,30:60,:);
+%
+%%incoming_temp_ts=temp_sfc_array_am3(:,30:60,:,ensnum);
+%%incoming_eis_ts=eis_array_am3(:,30:60,:,ensnum);
+%%incoming_lcc_ts=lcloud_array_am3(:,30:60,:,ensnum);
 
 clear var_array;
 clear cc;
 lcc_mlr
 
+incoming_ts=primary_del_ymn;
+running_mean;
+lcc_am3_smooth=output_ts;
+
 figure
-plot(lcc_del_ymn,'k')
+plot(primary_del_ymn,'k')
 hold on
-zhou_ts=3.7.*eis_del_ymn-0.9.*sst_del_ymn;
+zhou_ts=3.7.*first_del_ymn-0.9.*second_del_ymn;
 plot(zhou_ts,'g')
-new_ts=cc(1).*eis_del_ymn+cc(2).*sst_del_ymn;
+new_ts=cc(1).*first_del_ymn+cc(2).*second_del_ymn;
 plot(new_ts,'r')
-%zhou_am3=corrcoef(lcc_del_ymn,zhou_ts)
-%new_am3=corrcoef(lcc_del_ymn,new_ts)
 title('lcc change for AM3')
 
-incoming_temp_ts=temp_sfc_array_am4(:,60:120,:,ensnum);
-incoming_eis_ts=eis_array_am4(:,60:120,:,ensnum);
-incoming_lcc_ts=lcloud_array_am4(:,60:120,:,ensnum);
+incoming_ts=new_ts;
+running_mean;
+new_ts_am3_smooth=output_ts;
+
+
+%am4
+second_ts=temp_sfc_am4_mn(:,60:120,:);
+first_ts=eis_ens_am4_mn(:,60:120,:);
+primary_ts=lcloud_am4_mn(:,60:120,:);
+
+%second_ts=temp_sfc_array_am4(:,60:120,:,ensnum);
+%first_ts=eis_array_am4(:,60:120,:,ensnum);
+%primary_ts=lcloud_array_am4(:,60:120,:,ensnum);
 
 clear var_array;
 clear cc;
 lcc_mlr
 
+incoming_ts=primary_del_ymn;
+running_mean;
+lcc_am4_smooth=output_ts;
 
 figure
-plot(lcc_del_ymn,'k')
+plot(primary_del_ymn,'k')
 hold on
-zhou_ts=3.7.*eis_del_ymn-0.9.*sst_del_ymn;
+zhou_ts=3.7.*first_del_ymn-0.9.*second_del_ymn;
 plot(zhou_ts,'g')
-new_ts=cc(1).*eis_del_ymn+cc(2).*sst_del_ymn;
+new_ts=cc(1).*first_del_ymn+cc(2).*second_del_ymn;
 plot(new_ts,'r')
-zhou_am4=corrcoef(lcc_del_ymn,zhou_ts)
-new_am4=corrcoef(lcc_del_ymn,new_ts)
+zhou_am4=corrcoef(primary_del_ymn,zhou_ts)
+new_am4=corrcoef(primary_del_ymn,new_ts)
 title('lcc change for AM4')
 
+incoming_ts=new_ts;
+running_mean;
+new_ts_am4_smooth=output_ts;
 
+% multi-model figure
+figure
 
+timearr=1876:2001;
+plot(timearr,lcc_am2_smooth,'b','LineWidth',2)
+hold on
+plot(timearr,new_ts_am2_smooth,'b')
+plot(timearr,lcc_am3_smooth,'r','LineWidth',2)
+plot(timearr,new_ts_am3_smooth,'r')
+plot(timearr,lcc_am4_smooth,'k','LineWidth',2)
+plot(timearr,new_ts_am4_smooth,'k')
+title('LCC change and Approximate LCC change')
 
+%-------------------------------------------------------------
+% can we similarly decompose high cloud changes?  
+%-------------------------------------------------------------
+
+%am3
+second_ts=temp_sfc_am3_mn(:,30:60,:);
+first_ts=omega500_am3_mn(:,30:60,:);
+primary_ts=hcloud_am3_mn(:,30:60,:);
+
+clear var_array;
+clear cc;
+lcc_mlr
+
+incoming_ts=primary_del_ymn;
+running_mean;
+hcc_am3_smooth=output_ts;
+
+figure
+plot(primary_del_ymn,'k')
+hold on
+zhou_ts=3.7.*first_del_ymn-0.9.*second_del_ymn;
+plot(zhou_ts,'g')
+new_ts=cc(1).*first_del_ymn+cc(2).*second_del_ymn;
+plot(new_ts,'r')
+%zhou_am3=corrcoef(primary_del_ymn,zhou_ts)
+%new_am3=corrcoef(primary_del_ymn,new_ts)
+title('hcc change for AM3')
+
+incoming_ts=new_ts;
+running_mean;
+new_ts_am3_smooth=output_ts;
+
+figure
+plot(hcc_am3_smooth)
+hold on
+plot(new_ts_am3_smooth)
