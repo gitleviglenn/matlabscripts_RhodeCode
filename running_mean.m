b34d578f48cd41b01dbefebea5bb9074eefdf9d4
silvers <silvers@princeton.edu>
2017-08-18 16:19:27 -0400

    running_mean.m running_mean_30yr.m

--- running_mean.m
+++ running_mean.m
@@ -1,11 +1,15 @@
-
+%-----------------------------------------------------------------------------------------
+% running_mean.m
+%
+% smooths an incoming time series with a 9 point running mean
+%
+% levi silvers                                                           june 2017
 %-----------------------------------------------------------------------------------------
 %tend_a is presumably the end of the series
 % tend_a = iend_a-istart_a;
 %incoming_ts=v.sst_mn_ts_a;
 % compute a running mean
 rough_ts=incoming_ts;
-%tendindex=tend_a;
 % for a 13 point running mean
 %for ti=7:tendindex-6
 %  ts_smooth(ti-6)=(rough_ts(ti-6)+rough_ts(ti-5)+rough_ts(ti-4)+rough_ts(ti-3)+rough_ts(ti-2)+rough_ts(ti-1)+rough_ts(ti)+rough_ts(ti+1)+rough_ts(ti+2)+rough_ts(ti+3)+rough_ts(ti+4)+rough_ts(ti+5)+rough_ts(ti+6))/13.;
@@ -17,5 +21,4 @@ end
 output_ts=ts_smooth;
 clear rough_ts; clear ts_smooth;
 %-----------------------------------------------------------------------------------------
-%sfc_t_sm_ts_a=output_ts;
 %
