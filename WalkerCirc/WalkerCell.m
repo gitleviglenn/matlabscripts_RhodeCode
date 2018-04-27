7ce17fcb91cce24a163e1e83de6d7a453e894a83
silvers <silvers@princeton.edu>
2018-04-26 21:50:47 -0400

    lost of changes to WalkerCell and WalkerEnergetics

--- WalkerCirc/WalkerCell.m
+++ WalkerCirc/WalkerCell.m
@@ -1,6 +1,6 @@
 path_base='/Users/silvers/data/WalkerCell/'
 %tit_st=' cos sq ';
-tit_st=' gaussian ';
+%tit_st=' gaussian ';
 %path_25km=path;
 
 path=strcat(path_base,'gauss_d/');
@@ -11,12 +11,21 @@ path=strcat(path_base,'gauss_d/');
 %init_st_fname=strcat(path,'c96L33_8x80_nh.19790101');
 
 % original paths
-%path_25km=strcat(path,'c96L33_8x80_nh.19790101');
-%path_2km=strcat(path,'c96L33_100x1000_nh_crm.19790101');
+tit_st='4K ent1p1';
 
-%path_25km=strcat(path,'am4p0_8x160_4K/','19790101');
-%path_25km=strcat(path,'am4p0_8x160_4K_nocgdrag/','19790101');
-path_25km=strcat(path,'c96L33_am4p0_8x160_nh_25km_wlkr_4K_c/','19790101');
+% timing variables for gcm 
+an_t1=4; % for months
+an_t2=12;
+
+an_t1_d=120;
+an_t2_d=365;
+
+% timing variables for crm [days]
+t_end=28; % was 59 original
+t_mid=1; % was 30 originally
+
+% at least for now, our control is wlkr_4K_b
+path_25km=strcat(path,'c96L33_am4p0_8x160_nh_25km_wlkr_4K_ent1p1/','19790101');
 path_2km=strcat(path,'am4p0_50x2000_4K/','19790301');
 
 source_25km=strcat(path_25km,'.atmos_daily.nc');
@@ -29,97 +38,41 @@ source_25km_8xday=strcat(path_25km,'.atmos_8xdaily.nc');
 source_2km_8xday=strcat(path_2km,'.atmos_8xdaily.nc');
 
 % domain related parameters:
-%blah=1:1000;
-%blah=1:500;
-%x80=0:25:2000;
 xgcm=0:25:4000; % size of gcm domain in km
 xgcm_ngp=160; % gcm number of grid points in x
-%x1000=0:2:2000;
 xcrm=0:2:4000; % size of crm domain in km
-%xcrm=0:2:2000;
 xcrm_ngp=2000; % crm number of grid points in x
 
 % latent heat of vaporization
 latheat=2.26e6 % J/kg
 
-% timing variables for crm [days]
-t_end=28; % was 59 originally
-t_mid=1; % was 30 originally
+% scaling factors
+% precip can be converted into mm/day (scale1) 
+% or energy units of W/m2 (scale2)
+scale1=86400.; % s m^2 mm / kg day
+scale2=2.265e-6; % J/kg
+scale=scale1;
+cltscale=100. % convert to percentage of cloud fraction
 
-% read variables
+% read variables from input files
+
+% precipitation
 precip_25km=ncread(source_25km,'precip');
 precip_2km=ncread(source_2km,'precip');
 precip_25km_8x=ncread(source_25km_8xday,'precip');
 precip_2km_8x=ncread(source_2km_8xday,'precip');
-w_25km=ncread(source_25km,'w');
-w_2km=ncread(source_2km,'w');
-w500_25km=ncread(source_25km,'w500');
-w500_2km=ncread(source_2km,'w500');
-clt_2km=ncread(source_2km,'cld_amt');
-clt_25km=ncread(source_25km,'cld_amt');
-liq_2km=ncread(source_2km_month,'tot_liq_amt');
-liq_25km=ncread(source_25km_month,'tot_liq_amt');
-ice_2km=ncread(source_2km_month,'tot_ice_amt');
-ice_25km=ncread(source_25km_month,'tot_ice_amt');
-hur_2km=ncread(source_2km_month,'rh');
-hur_25km=ncread(source_25km_month,'rh');
-pfull_2km=ncread(source_2km,'pfull');
-tsurf=ncread(source_25km,'t_surf');
-
-temp_25km=ncread(source_25km_month,'temp');
-zfull_25km=ncread(source_2km_month,'z_full');
-pfull_25km=ncread(source_2km_month,'pfull');
-
-
-liq_2km_tot=liq_2km+ice_2km;
-liq_25km_tot=liq_25km+ice_25km;
-liq_2km_zmn=squeeze(mean(liq_2km_tot,2));
-liq_25km_zmn=squeeze(mean(liq_25km_tot,2));
-
-liq_25km_zmn_last10m=liq_25km_zmn(:,:,2:12);
-liq_25km_zmn_1m=squeeze(mean(liq_25km_zmn_last10m,3));
-
-% relative humidity
-% for the 2km runs, the time dimension is often not present becuase it is 1
-
-hur_2km_zmn=squeeze(mean(hur_2km,2));
-hur_25km_zmn=squeeze(mean(hur_25km,2));
-
-hur_2km_zmn_1m=hur_2km_zmn;
-hur_25km_zmn_last10m=hur_25km_zmn(:,:,2:12);
-hur_25km_zmn_1m=squeeze(mean(hur_25km_zmn_last10m,3));
-
-
-q_25km=ncread(source_25km_month,'sphum');
-q_25km_zmn=squeeze(mean(q_25km,2));
-%q_25km_zmn_end=q_25km_zmn(:,:,3:12);
-q_25km_zmn_end=q_25km_zmn(:,:,2);
-q_25km_zmntmn=mean(q_25km_zmn_end,3);
-
-q_2km=ncread(source_2km_month,'sphum');
-q_2km_zmn=squeeze(mean(q_2km,2));
-%q_2km_zmn_end=q_2km_zmn(:,:,2);
-%q_2km_zmntmn=mean(q_2km_zmn_end,3);
-
-q_2km_zmntmn=squeeze(q_2km_zmn(:,:,1));
-
-% begin to process variables
+p_cv_25km=ncread(source_25km,'prec_conv'); %kg(h2o)/m2/2
+p_ls_25km=ncread(source_25km,'prec_ls');
+p_cv_2km=ncread(source_2km,'prec_conv'); %kg(h2o)/m2/2
+p_ls_2km=ncread(source_2km,'prec_ls');
 
 precip_25km_znm=mean(precip_25km,2);
 precip_2km_znm=mean(precip_2km,2);
 precip_25km_8x_znm=mean(precip_25km_8x,2);
 precip_2km_8x_znm=mean(precip_2km_8x,2);
 
-tsurf1=squeeze(tsurf(:,4,300)); % indices shouldn't matter here...
-
-% precip can be converted into mm/day (scale1) 
-% or energy units of W/m2 (scale2)
-scale1=86400.; % s m^2 mm / kg day
-scale2=2.265e-6; % J/kg
-scale=scale1;
-
-p_contours=[10.,20.,30.,40.,50.,60.,70.,80.,90.,100.];
-p_contours=[5.,20.,35.,50.,65.,80.,95.,110.,125.,140.];
+%p_contours=[10.,20.,30.,40.,50.,60.,70.,80.,90.,100.];
+%p_contours=[5.,20.,35.,50.,65.,80.,95.,110.,125.,140.];
 
 %precip_25km_znm=mean(precip_25km,2);
 %precip_25km_8x=precip_25km_8x(:,:,4800:2920);
@@ -131,17 +84,76 @@ p_2km_8x_znm=scale.*(squeeze(precip_2km_8x_znm));
 p_25km_znm=scale.*(squeeze(precip_25km_znm));
 p_25km_8x_znm=scale.*(squeeze(precip_25km_8x_znm));
 
-%p_25km_8x_znm=p_25km_8x_znm(:,4800:2920);
 % time mean precip
-p_25km_8x_znm_equil=p_25km_8x_znm(:,481:2920);
+p_25km_8x_znm_equil=p_25km_8x_znm(:,960:2920);
 p_25km_tmean=mean(p_25km_8x_znm_equil,2);
 p_2km_tmean=mean(p_2km_8x_znm,2);
 
-cltscale=100. % convert to percentage of cloud fraction
+% vertical velocity
+w_25km=ncread(source_25km,'w');
+w_2km=ncread(source_2km,'w');
+w500_25km=ncread(source_25km,'w500');
+w500_2km=ncread(source_2km,'w500');
+
+clt_2km=ncread(source_2km,'cld_amt');
+clt_25km=ncread(source_25km,'cld_amt');
+
+liq_2km=ncread(source_2km_month,'tot_liq_amt');
+liq_25km=ncread(source_25km_month,'tot_liq_amt');
+
+ice_2km=ncread(source_2km_month,'tot_ice_amt');
+ice_25km=ncread(source_25km_month,'tot_ice_amt');
+
+hur_2km=ncread(source_2km_month,'rh');
+hur_25km=ncread(source_25km_month,'rh');
+
+pfull_2km=ncread(source_2km,'pfull');
+pfull_25km=ncread(source_2km_month,'pfull');
+
+tsurf=ncread(source_25km,'t_surf');
+temp_25km=ncread(source_25km_month,'temp');
+
+zfull_25km=ncread(source_2km_month,'z_full');
+zfull_25km_zmn=squeeze(mean(zfull_25km,2));
+zfull_25km_ztmn=squeeze(mean(zfull_25km_zmn,3));
+
+liq_2km_tot=liq_2km+ice_2km;
+liq_2km_zmn=squeeze(mean(liq_2km_tot,2));
+
+liq_25km_tot=liq_25km+ice_25km;
+liq_25km_zmn=squeeze(mean(liq_25km_tot,2));
+liq_25km_zmn_last10m=liq_25km_zmn(:,:,an_t1:an_t2);
+liq_25km_zmn_1m=squeeze(mean(liq_25km_zmn_last10m,3));
+
+% relative humidity
+% for the 2km runs, the time dimension is often not present becuase it is 1
+
+hur_2km_zmn=squeeze(mean(hur_2km,2));
+hur_25km_zmn=squeeze(mean(hur_25km,2));
+
+hur_2km_zmn_1m=hur_2km_zmn;
+hur_25km_zmn_last10m=hur_25km_zmn(:,:,an_t1:an_t2);
+hur_25km_zmn_1m=squeeze(mean(hur_25km_zmn_last10m,3));
+
+
+q_25km=ncread(source_25km_month,'sphum');
+q_25km_zmn=squeeze(mean(q_25km,2));
+q_25km_zmn_end=q_25km_zmn(:,:,an_t1:an_t2);
+%q_25km_zmn_end=q_25km_zmn(:,:,2);
+q_25km_zmntmn=mean(q_25km_zmn_end,3);
+
+q_2km=ncread(source_2km_month,'sphum');
+q_2km_zmn=squeeze(mean(q_2km,2));
+%q_2km_zmn_end=q_2km_zmn(:,:,2);
+%q_2km_zmntmn=mean(q_2km_zmn_end,3);
+
+q_2km_zmntmn=squeeze(q_2km_zmn(:,:,1));
+
+tsurf1=squeeze(tsurf(:,4,300)); % indices shouldn't matter here...
 
 clt_25km_znm=cltscale.*squeeze(mean(clt_25km,2));
 %clt_25km_znm_2m=clt_25km_znm(:,:,61:90);
-clt_25km_znm_2m=clt_25km_znm(:,:,31:360);
+clt_25km_znm_2m=clt_25km_znm(:,:,an_t1_d:an_t2_d);
 
 clt_2km_znm=cltscale.*squeeze(mean(clt_2km,2));
 clt_2km_znm_end=clt_2km_znm(:,:,t_mid:t_end);
@@ -168,12 +180,41 @@ w500_2km_zmn_tm=mean(w500_2km_zmn,2);
 
 % w at all levels
 w_25km_zmn=squeeze(mean(w_25km,2));
-w_25km_zmn_tmn_last11=squeeze(w_25km_zmn(:,:,21:364));
-w_25km_zmn_tm=squeeze(mean(w_25km_zmn_tmn_last11,3));
+w_25km_zmn_tmn_last8=squeeze(w_25km_zmn(:,:,an_t1_d:an_t2_d));
+w_25km_zmn_tm=squeeze(mean(w_25km_zmn_tmn_last8,3));
 w_2km_zmn=squeeze(mean(w_2km,2));
 w_2km_zmn_tm=mean(w_2km_zmn(:,:,t_mid:t_end),3); 
 
+q25=q_25km_zmntmn';
+q2=q_2km_zmntmn';
 
+% compute the potential temperature
+compTheta
+
+StreamFun
+
+% psi_mat=zeros(5,160,33);
+% WalkerCell
+% StreamFun
+% psi_mat(3,:,:)=psi_3(:,:);
+% WalkerCell
+% StreamFun
+% psi_mat(4,:,:)=psi_3(:,:);
+% etc....
+
+% p_mat=zeros(5,160,2920);
+% WalkerCell
+% p_mat(1,:,:)=p_25km_8x_znm(:,:);
+% WalkerCell
+% p_mat(2,:,:)=p_25km_8x_znm(:,:);
+% WalkerCell
+% p_mat(3,:,:)=p_25km_8x_znm(:,:);
+% WalkerCell
+% p_mat(4,:,:)=p_25km_8x_znm(:,:);
+% WalkerCell
+% p_mat(5,:,:)=p_25km_8x_znm(:,:);
+
+%-----------------------------------------------------------------------
 % figures
 
 % points at which to plot profiles
@@ -190,7 +231,7 @@ subplot(1,2,1)
 [C,h]=contourf(1:xcrm_ngp,pfull_2km,clt_2km_znm_end_tmn',clt_contours,'EdgeColor','None');
 v=[5,10,20,30,50,70];
 clabel(C,h,v);
-tit_a=strcat('cloud fraction 2km',tit_st)
+tit_a=strcat('cloud fraction 2km')
 title(tit_a)
 set(gca,'Ydir','reverse')
 colorbar
@@ -198,52 +239,12 @@ subplot(1,2,2)
 %caxis=([1 25]);
 [C,h]=contourf(1:xgcm_ngp,pfull_2km,clt_25km_znm_tmn',clt_contours,'EdgeColor','None');
 clabel(C,h,v);
-tit_b=strcat('cloud fraction 25km',tit_st)
+tit_b=strcat('cloud fraction 25km')
 title(tit_b)
 set(gca,'Ydir','reverse')
 colorbar
+suptitle(tit_st)
 
-% figure
-% subplot(2,2,1)
-% contourf(p_2km_znm',p_contours,'EdgeColor','None')
-% subplot(2,2,2)
-% contourf(p_25km_znm',p_contours,'EdgeColor','None')
-% colorbar
-% subplot(2,2,3)
-% contourf(p_25km_znm(:,1:59)',p_contours,'EdgeColor','None')
-% title('Precipitation Hovmuller Plots')
-% subplot(2,2,4)
-% plot(tsurf1)
-
-% figure
-% subplot(2,1,1)
-% contourf(p_2km_8x_znm',p_contours,'EdgeColor','None')
-% subplot(2,1,2)
-% contourf(p_25km_8x_znm(:,1:480)',p_contours,'EdgeColor','None')
-
-% figure
-% subplot(1,2,1)
-% plot(w_25km_532_zmn_tm)
-% axis([0 80 -0.02 0.05])
-% title('vertical velocity at 532 for 25km')
-% subplot(1,2,2)
-% plot(w_2km_532_zmn_tm)
-% axis([0 1000 -0.02 0.05])
-% title('vertical velocity at 532 for 2km')
-
-% wlev=18;
-% figure
-% subplot(1,2,1)
-% plot(w_25km_zmn_tm(:,wlev))
-% axis([0 80 -0.02 0.05])
-% title('vertical velocity 25km')
-% subplot(1,2,2)
-% plot(w_2km_zmn_tm(:,wlev))
-% axis([0 1000 -0.02 0.05])
-% title('vertical velocity 2km')
-
-q25=q_25km_zmntmn';
-q2=q_2km_zmntmn';
 
 figure
 plot(q25(:,gcm_ris),pfull_2km,'--r','LineWidth',2);
@@ -259,18 +260,18 @@ set(gca,'FontSize',16)
 tit_c=strcat('Specific Humidity',tit_st);
 title(tit_c)
 
-figure
-plot(xgcm(1:xgcm_ngp),tsurf1,'k','LineWidth',2);
-ylabel('Kelvin','FontSize',20)
-xlabel('km','FontSize',20)
-yt=get(gca,'YTick');
-set(gca,'FontSize',16)
-tit_d=strcat('Surface Temperature',tit_st);
-title(tit_d)
+% figure
+% plot(xgcm(1:xgcm_ngp),tsurf1,'k','LineWidth',2);
+% ylabel('Kelvin','FontSize',20)
+% xlabel('km','FontSize',20)
+% yt=get(gca,'YTick');
+% set(gca,'FontSize',16)
+% tit_d=strcat('Surface Temperature',tit_st);
+% title(tit_d)
 
 figure
 wlev=18;
-
+subplot(1,2,1)
 plot(xcrm(1:xcrm_ngp),w_2km_zmn_tm(:,wlev),'k','LineWidth',2);
 %plot(xcrm(1:xcrm_ngp),w500_2km_zmn_tm(:),'k','LineWidth',2);
 hold on
@@ -280,10 +281,10 @@ ylabel('w (m/s)','FontSize',20)
 xlabel('km','FontSize',20)
 yt=get(gca,'YTick');
 set(gca,'FontSize',16)
-tit_e=strcat('Vertical Velocity',tit_st);
+tit_e=strcat('Vertical Velocity');
 title(tit_e)
 
-figure
+subplot(1,2,2)
 % plot precip time mean
 plot(xcrm(1:xcrm_ngp),p_2km_tmean(:),'k','LineWidth',2);
 hold on
@@ -292,8 +293,12 @@ ylabel('Precip (mm/d)','FontSize',20)
 xlabel('km','FontSize',20)
 yt=get(gca,'YTick');
 set(gca,'FontSize',16)
-tit_e=strcat('Time Mean Precip',tit_st);
-title(tit_e)
+MeanPrecip_25km=mean(p_25km_tmean,1)
+MeanPrecip_2km=mean(p_2km_tmean,1)
+tit_e=strcat('Precip GCM:',num2str(MeanPrecip_25km),' CRM:',num2str(MeanPrecip_2km));
+title(tit_e); 
+suptitle(tit_st)
+
 
 figure
 liq_2km_zmn_scale=liq_2km_zmn*1000.;
@@ -304,18 +309,20 @@ caxis=([0.00001 0.03]);
 [C,h]=contourf(1:xcrm_ngp,pfull_2km,liq_2km_zmn_scale',q_contours,'EdgeColor','None');
 v=[0.03,0.01,0.001,0.0001,0.00001];
 clabel(C,h,v);
-tit_a=strcat('liq+ice water 2km',tit_st)
-title(tit_a)
+tit_a=strcat('liq+ice water 2km');
+title(tit_a);
 set(gca,'Ydir','reverse')
 colorbar
 subplot(1,2,2)
 caxis=([0.00001 0.03]);
 [C,h]=contourf(1:xgcm_ngp,pfull_2km,liq_25km_zmn_scale',q_contours,'EdgeColor','None');
 clabel(C,h,v);
-tit_b=strcat('liq+ice 25km',tit_st)
-title(tit_b)
+tit_b=strcat('liq+ice 25km');
+title(tit_b);
 set(gca,'Ydir','reverse')
 colorbar
+tit_liq=strcat('liq: ',tit_st);
+suptitle(tit_liq)
 
 figure
 rh_contours=[5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85];
@@ -324,8 +331,8 @@ caxis=([10 80]);
 [C,h]=contourf(1:xcrm_ngp,pfull_2km,hur_2km_zmn_1m',rh_contours,'EdgeColor','None');
 v=[10,20,30,50,70];
 clabel(C,h,v);
-tit_a=strcat('relative humidity 2km',tit_st)
-title(tit_a)
+tit_a=strcat('relative humidity 2km');
+title(tit_a);
 %set(gca,'Ydir','reverse')
 %colorbar
 hold on
@@ -337,7 +344,12 @@ subplot(1,2,2)
 caxis=([10 80]);
 [C,h]=contourf(1:xgcm_ngp,pfull_2km,hur_25km_zmn_1m',rh_contours,'EdgeColor','None');
 clabel(C,h,v);
-tit_b=strcat('relative humidity 25km',tit_st)
-title(tit_b)
+tit_b=strcat('relative humidity 25km');
+title(tit_b);
 set(gca,'Ydir','reverse')
 colorbar
+tit_rh=strcat('RH: ',tit_st);
+suptitle(tit_rh)
+
+%StreamFun
+%WalkerEnergetics
--- WalkerCirc/WalkerEnergetics.m
+++ WalkerCirc/WalkerEnergetics.m
@@ -8,6 +8,24 @@ conv=60*60*24; % convert to Kelvin per day
 evap_25km=ncread(source_25km_month,'evap');
 evap_2km=ncread(source_2km_month,'evap');
 
+% W/m2
+sh_25km=ncread(source_25km_month,'shflx');
+sh_2km=ncread(source_2km_month,'shflx');
+
+% convert to energy units of W/m2
+evap_25km_en=latheat.*evap_25km;
+evap_25km_en_zmn=squeeze(mean(evap_25km_en,2));
+evap_25km_en_ztmn=squeeze(mean(evap_25km_en_zmn,2));
+
+sh_25km_zmn=squeeze(mean(sh_25km,2));
+sh_25km_ztmn=squeeze(mean(sh_25km_zmn,2));
+
+evap_2km_en=latheat.*evap_2km;
+evap_2km_en_ztmn=squeeze(mean(evap_2km_en,2));
+
+sh_2km_zmn=squeeze(mean(sh_2km,2));
+sh_2km_ztmn=squeeze(mean(sh_2km_zmn,2));
+
 % deg K/s
 tdtconv_25km=ncread(source_25km_month,'tdt_conv');
 tdtconv_2km=ncread(source_2km_month,'tdt_conv');
@@ -42,20 +60,24 @@ tdtvdif_2km=conv.*tdtvdif_2km;
 
 % det K/s
 tdtlw_25km=ncread(source_25km_month,'tdt_lw');
-tdtlw_2km=ncread(source_2km_month,'tdt_lw');
-tdtlw_25km=conv.*tdtlw_25km;
-tdtlw_2km=conv.*tdtlw_2km;
+tdtlw_25km=conv.*tdtlw_25km(:,:,:,an_t1:an_t2);
 tdtlw_25_tmn=squeeze(mean(tdtlw_25km,4));
 tdtlw_25_tzmn=squeeze(mean(tdtlw_25_tmn,2));
+
+tdtlw_2km=ncread(source_2km_month,'tdt_lw');
+tdtlw_2km=conv.*tdtlw_2km;
 tdtlw_2_tmn=squeeze(mean(tdtlw_2km,4));
 tdtlw_2_tzmn=squeeze(mean(tdtlw_2_tmn,2));
 % det K/s
 tdtsw_25km=ncread(source_25km_month,'tdt_sw');
+tdtsw_25km=conv.*tdtsw_25km(:,:,:,an_t1:an_t2);
+tdtsw_25_tmn=squeeze(mean(tdtsw_25km,4));
+tdtsw_25_tzmn=squeeze(mean(tdtsw_25_tmn,2));
+
 tdtsw_2km=ncread(source_2km_month,'tdt_sw');
-tdtsw_25km=conv.*tdtsw_25km;
 tdtsw_2km=conv.*tdtsw_2km;
-tdtsw_tmn=squeeze(mean(tdtsw_25km,4));
-tdtsw_tzmn=squeeze(mean(tdtsw_tmn,2));
+tdtsw_2_tmn=squeeze(mean(tdtsw_2km,4));
+tdtsw_2_tzmn=squeeze(mean(tdtsw_2_tmn,2));
 
 % W/m2  net lw flux (at the surface i think)
 lwflx_25km=ncread(source_25km_month,'lwflx');
@@ -96,58 +118,160 @@ heatrad_2km=ncread(source_2km_month,'heat2d_rad');
 heatsw_25km=ncread(source_25km_month,'heat2d_sw');
 heatsw_2km=ncread(source_2km_month,'heat2d_sw');
 
+%compTheta
+
+stst=zeros(160,33);
+app_R=zeros(160,33);
+
+for j=2:32
+    for i=1:160
+        stst(i,j)=(.102/rho_25km(i,j)).*(temp_znm(i,j)/theta_znm(i,j).*((theta_znm(i,j+1)-theta_znm(i,j-1))/(zfull_25km_ztmn(i,j+1)-zfull_25km_ztmn(i,j-1))));
+        %stst(i,j)=-(temp_znm(i,j)/theta_znm(i,j).*((theta_znm(i,j+1)-theta_znm(i,j-1))/(pfull_25km(j+1)-pfull_25km(j-1))));
+    end 
+end
+
+for i=1:160
+    %stst(i,1)=-(.1/rho_25km(i,j)).*(temp_znm(i,1)/theta_znm(i,1).*((theta_znm(i,2)-theta_znm(i,1))/(pfull_25km(2)-pfull_25km(1))));
+    %stst(i,33)=-(.1/rho_25km(i,j)).*(temp_znm(i,33)/theta_znm(i,33).*((theta_znm(i,33)-theta_znm(i,32))/(pfull_25km(33)-pfull_25km(32))));
+    stst(i,1)=(.102/rho_25km(i,1)).*(temp_znm(i,1)/theta_znm(i,1).*((theta_znm(i,2)-theta_znm(i,1))/(zfull_25km_ztmn(i,2)-zfull_25km_ztmn(i,1))));
+    stst(i,33)=(.102/rho_25km(i,33)).*(temp_znm(i,33)/theta_znm(i,33).*((theta_znm(i,33)-theta_znm(i,32))/(zfull_25km_ztmn(i,33)-zfull_25km_ztmn(i,32))));
+end 
+
+%stst_conv=stst*conv;
+%app_R=w_25km_ztmn./stst;
+app_R=conv*w_25km_ztmn.*stst;   % units?  should be K/day
+
+rad_heating=tdtlw_25_tzmn+tdtsw_25_tzmn;
+
+%---------------------------------------------------------------
+% FIGURES ----------------------------------------------
+
 figure
-subplot(3,2,1)
-heating_cons=[-1.,-0.5,0.0,0.5,1.0,1.5,2.,2.5,3.];
-[C,h]=contourf(1:xgcm_ngp,pfull_2km,tdtconv_25km_tzmn',heating_cons);
-v=[-0.5,0.0,0.5,1.0,2.0,3.0]; % if labels are desired on contours
-clabel(C,h,v);
-title('convective heating K/d')
-set(gca,'Ydir','reverse')
-colorbar
-
-% figure
-% subplot(3,2,2)
-% heating_cons=[-1.,-0.5,0.0,0.5,1.0,1.5,2.,2.5,3.];
-% [C,h]=contourf(1:xcrm_ngp,pfull_2km,tdtconv_2km_tzmn',heating_cons);
-% v=[-0.5,0.0,0.5,1.0,2.0,3.0]; % if labels are desired on contours
-% clabel(C,h,v);
-% title('convective heating K/d')
-% set(gca,'Ydir','reverse')
-% colorbar
-
-subplot(3,2,3)
-heating_cons=[-5.0,-3.,-2.5,-2.0,-1.5,-1.,-0.5,0.0,0.5,1.0,1.5,2.,2.5,3.];
-[C,h]=contourf(1:xgcm_ngp,pfull_2km,tdtls_25km_tzmn',heating_cons);
-v=[-3.5,-2.5,-1.5,-0.5,0.0,0.5,1.0,2.0]; % if labels are desired on contours
-clabel(C,h,v);
-title('strat cloud heating K/d')
-set(gca,'Ydir','reverse') 
-colorbar
-
-subplot(3,2,4)
-heating_cons=[-5.0,-3.,-2.5,-2.0,-1.5,-1.,-0.5,0.5,1.0,1.5,2.,2.5,3.];
-[C,h]=contourf(1:xcrm_ngp,pfull_2km,tdtls_2km_tzmn',heating_cons);
-%v=[-3.5,-2.5,-1.5,-0.5,0.5,1.0,2.0]; % if labels are desired on contours
-%clabel(C,h,v);
-title('strat cloud heating K/d')
-set(gca,'Ydir','reverse') 
-colorbar
-
-subplot(3,2,5)
+subplot(3,3,1)
 heating_cons=[-5.0,-3.,-2.5,-2.0,-1.5,-1.,-0.5,0.0,0.5,1.0,1.5,2.,2.5,3.];
 [C,h]=contourf(1:xgcm_ngp,pfull_2km,tdtlw_25_tzmn',heating_cons);
 v=[-3.5,-2.5,-1.5,-0.5,0.0,0.5,1.0,2.0]; % if labels are desired on contours
 clabel(C,h,v);
-title('lw heating K/d')
+title('GCM: lw heating K/d')
 set(gca,'Ydir','reverse')
 colorbar
 
-subplot(3,2,6)
+subplot(3,3,4)
+heating_cons=[-5.0,-3.,-2.5,-2.0,-1.5,-1.,-0.5,0.0,0.5,1.0,1.5,2.,2.5,3.];
+[C,h]=contourf(1:xgcm_ngp,pfull_2km,tdtsw_25_tzmn',heating_cons);
+v=[-3.5,-2.5,-1.5,-0.5,0.0,0.5,1.0,2.0]; % if labels are desired on contours
+clabel(C,h,v);
+title('GCM: sw heating K/d')
+set(gca,'Ydir','reverse')
+colorbar
+
+subplot(3,3,7)
+plot(xgcm(1:xgcm_ngp),evap_25km_en_ztmn,'r','LineWidth',1.5);
+hold on;
+plot(xgcm(1:xgcm_ngp),sh_25km_ztmn,'r','LineWidth',1.5);
+plot(xcrm(1:xcrm_ngp),evap_2km_en_ztmn,'k','LineWidth',1.5);
+plot(xcrm(1:xcrm_ngp),sh_2km_ztmn,'k','LineWidth',1.5);
+title('Surface Fluxes: W/m2')
+
+%
+subplot(3,3,2)
+heating_cons=[-5.0,-3.,-2.5,-2.0,-1.5,-1.,-0.5,0.0,0.5,1.0,1.5,2.,2.5,3.];
+[C,h]=contourf(1:xgcm_ngp,pfull_2km,tdtls_25km_tzmn',heating_cons);
+v=[-3.5,-2.5,-1.5,-0.5,0.0,0.5,1.0,2.0]; % if labels are desired on contours
+clabel(C,h,v);
+title('GCM: strat cloud heating K/d')
+set(gca,'Ydir','reverse') 
+colorbar
+
+subplot(3,3,5)
+heating_cons=[-1.,-0.5,0.0,0.5,1.0,1.5,2.,2.5,3.];
+[C,h]=contourf(1:xgcm_ngp,pfull_2km,tdtconv_25km_tzmn',heating_cons);
+v=[-0.5,0.0,0.5,1.0,2.0,3.0]; % if labels are desired on contours
+clabel(C,h,v);
+title('GCM: convective heating K/d')
+set(gca,'Ydir','reverse')
+colorbar
+
+tdt_total_cloud=tdtconv_25km_tzmn+tdtls_25km_tzmn;
+
+subplot(3,3,8)
+heating_cons=[-1.,-0.5,0.0,0.5,1.0,1.5,2.,2.5,3.];
+[C,h]=contourf(1:xgcm_ngp,pfull_2km,tdt_total_cloud',heating_cons);
+v=[-0.5,0.0,0.5,1.0,2.0,3.0]; % if labels are desired on contours
+clabel(C,h,v);
+title('GCM: conv + ls heating K/d')
+set(gca,'Ydir','reverse')
+colorbar
+
+subplot(3,3,9)
+heating_cons=[-5.0,-3.,-2.5,-2.0,-1.5,-1.,-0.5,0.5,1.0,1.5,2.,2.5,3.];
+[C,h]=contourf(1:xcrm_ngp,pfull_2km,tdtls_2km_tzmn',heating_cons);
+%v=[-3.5,-2.5,-1.5,-0.5,0.5,1.0,2.0]; % if labels are desired on contours
+clabel(C,h,v);
+title('CRM: strat cloud heating K/d')
+set(gca,'Ydir','reverse') 
+colorbar
+
+subplot(3,3,6)
+heating_cons=[-5.0,-3.,-2.5,-2.0,-1.5,-1.,-0.5,0.0,0.5,1.0,1.5,2.,2.5,3.];
+[C,h]=contourf(1:xcrm_ngp,pfull_2km,tdtsw_2_tzmn',heating_cons);
+v=[-3.5,-2.5,-1.5,-0.5,0.0,0.5,1.0,2.0]; % if labels are desired on contours
+clabel(C,h,v);
+title('CRM: sw heating K/d')
+set(gca,'Ydir','reverse')
+colorbar
+tit_en=strcat('Energetics: ',tit_st);
+suptitle(tit_en)
+
+subplot(3,3,3)
 heating_cons=[-5.0,-3.,-2.5,-2.0,-1.5,-1.,-0.5,0.0,0.5,1.0,1.5,2.,2.5,3.];
 [C,h]=contourf(1:xcrm_ngp,pfull_2km,tdtlw_2_tzmn',heating_cons);
 v=[-3.5,-2.5,-1.5,-0.5,0.0,0.5,1.0,2.0]; % if labels are desired on contours
 clabel(C,h,v);
-title('lw heating K/d')
+title('CRM: lw heating K/d')
 set(gca,'Ydir','reverse')
 colorbar
+
+%------------------------
+
+figure
+subplot(2,2,1)
+heating_cons=[-5.0,-3.,-2.5,-2.0,-1.5,-1.,-0.5,0.0,0.5,1.0,1.5,2.,2.5,3.];
+[C,h]=contourf(1:xgcm_ngp,pfull_2km,tdtsw_25_tzmn',heating_cons);
+v=[-3.5,-2.5,-1.5,-0.5,0.0,0.5,1.0,2.0]; % if labels are desired on contours
+clabel(C,h,v);
+title('GCM: sw heating K/d')
+set(gca,'Ydir','reverse')
+colorbar
+
+subplot(2,2,2)
+heating_cons=[-5.0,-3.,-2.5,-2.0,-1.5,-1.,-0.5,0.0,0.5,1.0,1.5,2.,2.5,3.];
+[C,h]=contourf(1:xgcm_ngp,pfull_2km,tdtlw_25_tzmn',heating_cons);
+v=[-3.5,-2.5,-1.5,-0.5,0.0,0.5,1.0,2.0]; % if labels are desired on contours
+clabel(C,h,v);
+title('GCM: lw heating K/d')
+set(gca,'Ydir','reverse')
+colorbar
+
+subplot(2,2,3)
+heating_cons=[-1.5,-1.25,-1.0,-0.75,-.5,-.25,0.0,0.25,0.5,0.75,1.,1.25,1.5];
+[C,h]=contourf(1:xgcm_ngp,pfull_2km,rad_heating',heating_cons);
+v=[-1.5,-1.0,-.5,0.0,0.5,1.0,1.5]; % if labels are desired on contours
+caxis=([-1.5 1.25]);
+clabel(C,h,v);
+title('GCM: heating from lw + sw (K/d)')
+set(gca,'Ydir','reverse')
+colorbar
+
+subplot(2,2,4)
+heating_cons=[-1.5,-1.25,-1.0,-0.75,-.5,-.25,0.0,0.25,0.5,0.75,1.,1.25,1.5];
+[C,h]=contourf(1:xgcm_ngp,pfull_2km,app_R',heating_cons);
+v=[-1.5,-1.0,-.5,0.0,0.5,1.0,1.5]; % if labels are desired on contours
+caxis=([-1.5 1.25]);
+clabel(C,h,v);
+title('GCM: app heating, S*w (K/d)?')
+set(gca,'Ydir','reverse')
+colorbar
+
+tit_en=strcat('Energetics: ',tit_st);
+suptitle(tit_en)
