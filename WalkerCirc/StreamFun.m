4f4e49086fedaa42adff90acc6492a30882d5ac5
silvers <silvers@princeton.edu>
2018-06-27 16:40:42 -0400

    better comments

--- WalkerCirc/StreamFun.m
+++ WalkerCirc/StreamFun.m
@@ -1,4 +1,24 @@
-path_base='/Users/silvers/data/WalkerCell/'
+%----------------------------------------------------------------------
+% StreamFun.m
+%
+% computes mass streamfunction (psi_3, psi_crm_3) for 25km and 2km data
+%
+% psi depends on: w,u,sphum,temp, and pfull
+%
+% psi is computed as: psi(rho,w,u)=dx*rho(i,j)*w_ztmn(i,j)+u_bc_p;
+% 
+% The density rho is computed with the virtual temperature Tv
+% rho(i,j)=pfull(j)/(r_dry*Tv(i,j));
+% Tv=temp_ztmn.*(1+sphum_ztmn./epsilon)./(1+sphum_ztmn);
+% where sphum referes to the specific humidity
+%
+% The boundary condition in u is: 
+%      u_bc_p=sum(u_bc); --> sum over vertical levels where u_bc is:
+%      u_bc(jj)=(1/grav)*u_ztmn(i,jj)*(pfull(jj)-pfull(jj-1));
+%
+% levi silvers                                               june 2018
+%----------------------------------------------------------------------
+%path_base='/Users/silvers/data/WalkerCell/'
 
 % source_25km=strcat(path_25km,'.atmos_daily.nc');
 % source_2km=strcat(path_2km,'.atmos_daily.nc');
@@ -52,6 +72,7 @@ temp_25km_ztmn=squeeze(mean(temp_25km_zmn_eq,3));
 temp_25km_ztzmn=squeeze(mean(temp_25km_ztmn,1));
 
 temp_crm_zmn=squeeze(mean(temp_2km,2));
+temp_crm_zmn=temp_crm_zmn(:,:,4:6);
 temp_crm_ztmn=squeeze(mean(temp_crm_zmn,3));
 temp_crm_ztzmn=squeeze(mean(temp_crm_ztmn,1));
 
@@ -61,7 +82,8 @@ u_25km_zmn_eq=u_25km_zmn(:,:,an_t1:an_t2);
 u_25km_ztmn=squeeze(mean(u_25km_zmn_eq,3));
 
 u_2km_zmn=squeeze(mean(u_2km,2));
-u_2km_ztmn=u_2km_zmn;
+u_2km_zmn=u_2km_zmn(:,:,4:6);
+u_2km_ztmn=squeeze(mean(u_2km_zmn,3));
 
 % compute the time and zonal mean of w
 w_25km_zmn=squeeze(mean(w_25km,2));
@@ -70,7 +92,7 @@ w_25km_zmn_eq=w_25km_zmn(:,:,an_t1_d:an_t2_d);
 w_25km_ztmn=squeeze(mean(w_25km_zmn_eq,3));
 
 w_2km_zmn=squeeze(mean(w_2km,2));
-%w_2km_zmn=squeeze(w_2km(:,35,:,:));
+w_2km_zmn=w_2km_zmn(:,:,t_mid:t_end);
 w_2km_ztmn=squeeze(mean(w_2km_zmn,3));
 
 sphum_25km_zmn=squeeze(mean(sphum_25km,2));
@@ -79,7 +101,7 @@ sphum_25km_zmn_eq=sphum_25km_zmn(:,:,an_t1:an_t2);
 sphum_25km_ztmn=squeeze(mean(sphum_25km_zmn_eq,3));
 
 sphum_2km_zmn=squeeze(mean(sphum_2km,2));
-%sphum_2km_zmn=squeeze(sphum_2km(:,4,:,:));
+sphum_2km_zmn=sphum_2km_zmn(:,:,4:6);
 sphum_2km_ztmn=squeeze(mean(sphum_2km_zmn,3));
 
 % compute virtual temperature for use in the density computation
@@ -109,7 +131,7 @@ psi_crm_3=zeros(2000,33);
 
 % boundary condition depending on u wind
 u_bc=zeros(33,1);
-u_bc(1)=(1/9.8)*u_25km_ztmn(1,1)*(pfull_25km(2)-pfull_25km(1));
+u_bc(1)=(1/grav)*u_25km_ztmn(1,1)*(pfull_25km(2)-pfull_25km(1));
 
 for j=33:-1:2
    psi_3(1,j)=psi_3(160,j)+dx*rho_25km(1,j)*w_25km_ztmn(1,j);
@@ -125,7 +147,7 @@ for j=33:-1:2
    end
 end
     
-u_bc(1)=(1/9.8)*u_2km_ztmn(1,1)*(pfull_25km(2)-pfull_25km(1));
+u_bc(1)=(1/grav)*u_2km_ztmn(1,1)*(pfull_25km(2)-pfull_25km(1));
 
 
 for j=33:-1:2
