1a87357ebfcc683e1e03f71d1c1d89f1f8ead020
silvers <silvers@princeton.edu>
2018-10-09 15:10:11 -0400

    addition of switch getts to compute time series of radiative fluxes

--- toa_rad.m
+++ toa_rad.m
@@ -1,7 +1,9 @@
 %----------------------------------------------------------------------
 % toa_rad.m
 %
-% compute the net radiative flux at toa
+% compute the net radiative flux at toa and at the surface.  
+% if the getts (get time series) switch is on then time series
+% of the budgets are also computed by calling glb_mon_mn_ts()
 %
 % for the record(as of sep 24, 2018, the atmospheric energy lack 
 % of conservation is: 
@@ -15,7 +17,7 @@
 %    2.2413    2.3575    2.3785    2.7769    2.2740
 %    2.9332    2.7004    3.0250    3.0396    2.8745
 %
-% levi silvers                sep 2018
+% levi silvers                                                 oct 2018
 %----------------------------------------------------------------------
 
 % if these variables haven't already been set by a calling 
@@ -39,13 +41,11 @@
 
 % call script that opens data file and reads variables
 % open_radflux % if using Matlab 2015 or later
-%if (wkstn=='true')
 switch wkstn_loc
   case 'wkstn'
-  open_radflux_m2009 % if using Matlab 2009
-%else
+    open_radflux_m2009 % if using Matlab 2009
   case 'macbook'
-  open_radflux % if using Matlab 2015 or later
+    open_radflux % if using Matlab 2015 or later
 end
 
 olr_dmn=domaintime_mn(olr);
@@ -86,5 +86,31 @@ sw_cre=swup_clr_dmn-swup_dmn; % positive for clouds warming
 lw_cre=olr_clr_dmn-olr_dmn; % positive for clouds warming
 net_cre=toa_R-toa_R_clr; % swdn_toa-olr-swup_toar+olr_clr+swup_toa_clr;
 
+%blahface=glb_mon_mn_ts(lwdn_sfc,60);
 
+% compute time series of fluxes
+% check the values of the indices used to read in the data: sind, eind
+% these are defined in open_radflux_m2009.m
+length=eind-sind+1;
+switch comp_ts
+  case 'getts'
+  lwup_toa_ts=glb_mon_mn_ts(olr,length);
+  lwup_clr_toa_ts=glb_mon_mn_ts(olr_clr,length);
+  swdn_toa_ts=glb_mon_mn_ts(swdn_toa,length);
+  swup_toa_ts=glb_mon_mn_ts(swup_toa,length);
+  swup_clr_toa_ts=glb_mon_mn_ts(swup_toa_clr,length);
+  
+  lwdn_sfc_ts=glb_mon_mn_ts(lwdn_sfc,length);
+  swdn_sfc_ts=glb_mon_mn_ts(swdn_sfc,length);
+  lwup_sfc_ts=glb_mon_mn_ts(lwup_sfc,length);
+  swup_sfc_ts=glb_mon_mn_ts(swup_sfc,length);
+  shflx_sfc_ts=glb_mon_mn_ts(shflx,length);
+  evap_ts=glb_mon_mn_ts(evap,length);
+  lhflux_sfc_ts=lat_evap.*evap_ts;
+
+  % compute budget time series
+  toa_R_ts=swdn_toa_ts-swup_toa_ts-lwup_toa_ts;
+  sfc_R_ts=-lhflux_sfc_ts-shflx_sfc_ts+lwdn_sfc_ts-lwup_sfc_ts+swdn_sfc_ts-swup_sfc_ts;
+  atm_imb_ts=toa_R_ts-sfc_R_ts;
+end
 
