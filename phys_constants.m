e681006990896fd7a178cc83ba7d640d40fc2ab9
silvers <silvers@princeton.edu>
2019-01-11 09:07:22 -0500

    compute and load physical constants

---
+++ phys_constants.m
@@ -0,0 +1,16 @@
+% latent heat of vaporization
+latheat=2.26e6 % J/kg 
+%Lv=2.500*10^6; % J/kg
+g=9.81; %m/s2
+cp=1000.; % J/(kg K) cp for dry air
+Ra=287.; % J/(kg K) gas constant for dry air
+Rv=461.; % J/(kg K) gas constant for water vapor
+T0=280.; % K surface temperature
+p0=100000.; % Pa
+rh0=0.8;
+kappa=Ra/cp;
+r_dry=Ra;
+epsilon=0.622;
+grav=9.81;
+a_earth=6350000;
+
