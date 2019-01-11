199a8a5ee26b2b2e8628b59d25c5d20a3a2dec9d
silvers <silvers@princeton.edu>
2019-01-11 09:07:50 -0500

    function that computes the lapse rate

---
+++ lapser.m
@@ -0,0 +1,11 @@
+function var = lapser(temp,rho,plot_lat,tsfc,psfc,press)
+% compute the lapse rate
+
+phys_constants
+
+for j=2:33-1
+    var(j)=(-grav*rho(plot_lat,j)).*(temp(plot_lat,j+1)-temp(plot_lat,j-1))/(press(j+1)-press(j-1));
+end
+var(1)=var(2);
+var(33)=(tsfc(plot_lat,4)-temp(plot_lat,33-1))/(psfc(plot_lat,4)-press(33-1));
+
