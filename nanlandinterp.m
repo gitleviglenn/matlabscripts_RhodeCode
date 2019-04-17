d416c844b5b02733302832f488822034f9a3fdfb
silvers <silvers@princeton.edu>
2019-04-17 15:25:02 -0400

    function for replacing points in the land sea mask as NaNs

---
+++ nanlandinterp.m
@@ -0,0 +1,21 @@
+function sst_o = nanlandinterp(sstin,landm,XN,YN)
+
+[X,Y]=meshgrid(1:288,1:180);
+%[XN,YN]=meshgrid(0.2:0.8:288,1:180);
+testland=interp2(X,Y,landm',XN,YN);
+
+onlyocean=zeros(size(testland));
+onlyocean(testland~=0)=-999.;
+onlyocean(onlyocean==0)=1.;
+onlyocean(onlyocean==-999.)=0.;
+
+% this results in land points having a value of 1.  There is a gradient, presumably for
+% grid cells that are partially land.
+
+% and now to translate land points to NaNs...
+%sst_1t=squeeze(sstin(1,:,:));
+sst_oc_only=sstin.*onlyocean';
+sst_oc_only(sst_oc_only==0.)=NaN;
+sst_o=sst_oc_only;
+end % end function get nanland
+
