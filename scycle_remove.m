aa132bf889cfa3f4e97e7d9e1b60ad018afa66e1
silvers <silvers@princeton.edu>
2019-04-23 16:32:23 -0400

    small changes to scycle_remove due to matlab versions switching dims

--- scycle_remove.m
+++ scycle_remove.m
@@ -4,16 +4,19 @@ tlength=nyears*12;
 
 month=repmat(1:12,1,nyears);
 
-latlon=false;
+latlon=true;
 if latlon
-  deseasoned_data=zeros(tlength,nlat,nlon);
+  %deseasoned_data=zeros(tlength,nlat,nlon);
+  deseasoned_data=zeros(nlon,nlat,tlength);
   for lat=1:1:nlat
     for lon=1:1:nlon
       for k=1:12
-        monthlymeans(k)=mean(seasonal_data(month==k,lat,lon));
+        %monthlymeans(k)=mean(seasonal_data(month==k,lat,lon));
+        monthlymeans(k)=mean(seasonal_data(lon,lat,month==k));
       end
       for k=1:12
-        deseasoned_data(month==k,lat,lon)=seasonal_data(month==k,lat,lon)-monthlymeans(k);
+        %deseasoned_data(month==k,lat,lon)=seasonal_data(month==k,lat,lon)-monthlymeans(k);
+        deseasoned_data(lon,lat,month==k)=seasonal_data(lon,lat,month==k)-monthlymeans(k);
       end
     end
   end
